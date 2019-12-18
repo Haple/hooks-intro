f34a68e09ce483453baf3694845d306015285eb9
Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>
2019-12-18 16:32:00 -0300

    Entendendo o useState
    Signed-off-by: Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [techs, setTech] = useState([
+    'ReactJS',
+    'React Native',
+    'NodeJS',
+    'React Hooks',
+  ]);
+  const [newTech, setNewTech] = useState('');
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTech([...techs, newTech]);
+    setNewTech('');
+  }
+  return (
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <form>
+        <input
+          placeholder="Nova tecnologia"
+          type="text"
+          value={newTech}
+          onChange={e => setNewTech(e.target.value)}
+        />
+        <button type="submit" onClick={handleSubmit}>
+          Adicionar
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default App;
